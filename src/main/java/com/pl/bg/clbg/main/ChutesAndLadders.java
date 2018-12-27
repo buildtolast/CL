93c2848b3f2b8bbde6e8576e6dd19131833b3a04
builtolast <justme@codrite.com>
2018-12-27 02:37:21 -0500

    refactorings

--- src/main/java/com/pl/bg/clbg/main/ChutesAndLadders.java
+++ src/main/java/com/pl/bg/clbg/main/ChutesAndLadders.java
@@ -2,7 +2,6 @@ package com.pl.bg.clbg.main;
 
 import com.pl.bg.BoardGame;
 import com.pl.bg.BoardGameFactory;
-import com.pl.bg.Config;
 import com.pl.bg.clbg.CLBoardFactory;
 import com.pl.bg.clbg.CLBoardGameFactory;
 import com.pl.bg.clbg.CLConfig;
@@ -12,19 +11,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Slf4j
-public class Application {
+public class ChutesAndLadders {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ChutesAndLadders.class, args);
 
         Integer[][] chuteBlocksConfig = new Integer[][]{{98, 78}, {95, 75}, {93, 73}, {87, 24}, {64, 60}, {62, 19}, {56, 53}, {49, 11}, {47, 26}, {16, 6}};
         Integer[][] ladderBlocksConfig = new Integer[][]{{1, 38}, {4, 14}, {9, 31}, {21, 42}, {28, 84}, {36, 44}, {51, 67}, {71, 91}, {80, 100}};
         CLConfig clBoardConfig = CLConfig.build(100, chuteBlocksConfig, ladderBlocksConfig);
 
-        playChutesAndLadder(clBoardConfig, 2, new String[]{"Eric", "Paul"});
+        play(clBoardConfig, 2, new String[]{"Eric", "Paul"});
     }
 
-    static void playChutesAndLadder(CLConfig config, int numPlayers, String[] names){
+    static void play(CLConfig config, int numPlayers, String[] names) {
         log.info(" ----- Game Begins ------- ");
         BoardGameFactory factory = new CLBoardGameFactory(new CLBoardFactory(config), numPlayers, names);
         BoardGame boardGame = factory.build();
